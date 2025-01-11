934cbdda4db8d08befb3dbfdd15c1e8151a5623d
Niki704 <151815881+Niki704@users.noreply.github.com>
2025-01-11 15:05:47 +0530

    Created ErrorResponse in DTO to change the message body and the status of the exception message.

--- src/main/java/lk/edu/mynibm/backend_spring/dto/ErrorResponse.java
+++ src/main/java/lk/edu/mynibm/backend_spring/dto/ErrorResponse.java
@@ -1,4 +1,16 @@
 package lk.edu.mynibm.backend_spring.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ErrorResponse {
+
+    private String message;
+    private String errorCode;
 }
