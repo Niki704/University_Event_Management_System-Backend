90a5bf7edef82f84237484671dd73da024b0efc0
Niki704 <151815881+Niki704@users.noreply.github.com>
2025-01-11 15:04:06 +0530

    Created global exception handler class to handle existing exceptions.

--- src/main/java/lk/edu/mynibm/backend_spring/exception/GlobalExceptionHandler.java
+++ src/main/java/lk/edu/mynibm/backend_spring/exception/GlobalExceptionHandler.java
@@ -1,4 +1,44 @@
 package lk.edu.mynibm.backend_spring.exception;
 
+import lk.edu.mynibm.backend_spring.dto.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+//    @ExceptionHandler(RuntimeException.class)
+//    public ResponseEntity<String> handleRuntimeException(RuntimeException ex) {
+//        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+//                .body("Internal Server Error: " + ex.getMessage());
+//    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ErrorResponse> handleRuntimeException(RuntimeException ex) {
+        ErrorResponse errorResponse = new ErrorResponse();
+        errorResponse.setMessage("Internal Server Error: " + ex.getMessage());
+        errorResponse.setErrorCode("500");
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
+    }
+
+
+    @ExceptionHandler(NullPointerException.class)
+    public ResponseEntity<String> handleNullPointerException(NullPointerException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body("Bad Request: " + ex.getMessage());
+    }
+
+    @ExceptionHandler(CustomNotFoundException.class)
+    public ResponseEntity<String> handleCustomNotFoundException(CustomNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body("Not Found: " + ex.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGenericException(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("An unexpected error occurred: " + ex.getMessage());
+    }
 }
