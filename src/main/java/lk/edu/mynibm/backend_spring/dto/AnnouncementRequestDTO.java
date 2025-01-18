561e3d5c49731efa27bc01fe7877ceb00deb9ca1
Niki704 <151815881+Niki704@users.noreply.github.com>
2025-01-18 21:16:28 +0530

    Added NotNull constraint and made it to request without an id.

--- src/main/java/lk/edu/mynibm/backend_spring/dto/AnnouncementRequestDTO.java
+++ src/main/java/lk/edu/mynibm/backend_spring/dto/AnnouncementRequestDTO.java
@@ -1,4 +1,32 @@
 package lk.edu.mynibm.backend_spring.dto;
 
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.Set;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class AnnouncementRequestDTO {
+    @NotNull
+    private String title;
+
+    @NotNull
+    private String content;
+
+    @NotNull
+    private LocalDate date;
+
+    private Integer eventID;
+
+    private Set<Integer> batchIDs;
+
+    private Set<Integer> studentIDs;
+
+    @NotNull
+    private Integer teacherID;
 }
