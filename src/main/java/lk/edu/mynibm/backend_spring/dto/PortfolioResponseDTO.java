7b92c7562c1e7acd9d61c548acc28e929080bb34
Niki704 <151815881+Niki704@users.noreply.github.com>
2025-01-18 17:38:54 +0530

    created date, last updated, portfolio_id values are generating through the backend and set to respond combining with the values of requestDTO

--- src/main/java/lk/edu/mynibm/backend_spring/dto/PortfolioResponseDTO.java
+++ src/main/java/lk/edu/mynibm/backend_spring/dto/PortfolioResponseDTO.java
@@ -1,4 +1,20 @@
 package lk.edu.mynibm.backend_spring.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class PortfolioResponseDTO {
+    private Integer userID;
+    private int portfolioID;
+    private LocalDate createdDate;
+    private LocalDate lastUpdated;
+    private List<String> skills;
+    private List<String> attachments;
 }
