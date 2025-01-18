fd5c6a00612aed125939430c93e257b3dcfa8dd7
Niki704 <151815881+Niki704@users.noreply.github.com>
2025-01-18 21:13:51 +0530

    id (primary key) sets to auto generate

--- src/main/java/lk/edu/mynibm/backend_spring/model/Announcement.java
+++ src/main/java/lk/edu/mynibm/backend_spring/model/Announcement.java
@@ -1,4 +1,40 @@
 package lk.edu.mynibm.backend_spring.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.Set;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class Announcement {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private LocalDate date;
+
+    private Integer eventID;
+
+    @ElementCollection
+    private Set<Integer> batchIDs;
+
+    @ElementCollection
+    private Set<Integer> studentIDs;
+
+    @Column(nullable = false)
+    private Integer teacherID;
+
 }
