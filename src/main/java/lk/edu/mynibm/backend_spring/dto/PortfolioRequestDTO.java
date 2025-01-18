2e3aa6fc1bffb0afa674650642d7e7c9838092bf
Niki704 <151815881+Niki704@users.noreply.github.com>
2025-01-18 17:36:20 +0530

    As the updated API, some data is not passing from the client side. but it should respond with some additional values. Understanding the requirements, i have used seperate DTOs for handle request data and response data

--- src/main/java/lk/edu/mynibm/backend_spring/dto/PortfolioRequestDTO.java
+++ src/main/java/lk/edu/mynibm/backend_spring/dto/PortfolioRequestDTO.java
@@ -1,4 +1,16 @@
 package lk.edu.mynibm.backend_spring.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class PortfolioRequestDTO {
+    private Integer userID;
+    private List<String> skills;
+    private List<String> attachments;
 }
